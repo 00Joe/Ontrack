59570255923e3ea3556d8cffabe5b2b2bb84b3f8
00Joe <Bowleyjw@plu.edu>
2016-04-05 11:04:55 -0700

    template html file

--- ontrack/views.py
+++ ontrack/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
-from .models import Post
-from .forms import PostForm
+
 
 # example view rendering
+# from .models import Post
+# from .forms import PostForm
 # def post_list(request):
 #     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 #     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -26,3 +27,6 @@ from .forms import PostForm
 #         form = PostForm()
 #
 #     return render(request, 'blog/post_edit.html', {'form': form})
+
+def index(request):
+    return render(request, 'ontrack/index.html')
